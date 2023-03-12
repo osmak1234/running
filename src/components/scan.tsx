e16edd8f434647c09a29efb173b7a995e8020b3a
osmak1234 <tadeashanus0631@gmail.com>
2023-03-12 15:24:35 +0100

    TODO, basic forms, backend api(test), components

---
+++ src/components/scan.tsx
@@ -0,0 +1,50 @@
+import { Html5QrcodeScanner, Html5Qrcode } from "html5-qrcode";
+import React, { useEffect } from "react";
+import { Box, Text } from "@chakra-ui/react";
+function Scan() {
+  let alreadyRuns = false;
+  let html5QrcodeScanner;
+  // can't run server side
+  useEffect(() => {
+    getCameras().catch((err) => console.log(err));
+  }),
+    [];
+
+  //TODO: close the camera after scan
+  //TODO: add a button to scan again
+  async function getCameras() {
+    // prevent from running multiple times
+    if (alreadyRuns) return;
+    alreadyRuns = true;
+    //generate qr code scannner
+    const devices = await Html5Qrcode.getCameras();
+    if (devices && devices.length) {
+      const config = { fps: 30, qrbox: 250 };
+      html5QrcodeScanner = new Html5QrcodeScanner(
+        "reader",
+        config,
+        /* verbose= */ false
+      );
+      html5QrcodeScanner.render(onScanSuccess, QrcodeErrorCallback);
+    }
+  }
+
+  function onScanSuccess(qrCodeMessage: string) {
+    //TODO: handle scan success
+    console.log(qrCodeMessage);
+
+    html5QrcodeScanner.clear().catch((e) => console.log(e));
+  }
+  function QrcodeErrorCallback(error: string) {
+    //TODO: handle scan error
+    error = error;
+  }
+
+  return (
+    <>
+      <Box id='reader' w='full' height='auto'></Box>
+    </>
+  );
+}
+
+export default Scan;
